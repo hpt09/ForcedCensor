95eb6a166d62b97961e3f9bb7183003d4198ef1c
Harpreet <hpt09@live.com>
2019-07-21 00:21:56 +1200

    Reduced output to console and forced overwrite

--- censor.sh
+++ censor.sh
@@ -11,28 +11,27 @@ fi
 outputString=`python time.py $2`
 
 > edits.txt
+rm output*mp3
 
 IFS=' ' # space is set as delimiter
 read -ra ADDR <<< "$outputString"
 index=0
 startTime=0
 for i in "${ADDR[@]}"; do 
-	echo index is $index
-	echo $i
     IFS='-' # dash is set as delimiter
 	read -ra ADDR <<< "$i"
 	IFS=' ' #resetting IFS value
 	endTime=${ADDR[0]}
-	ffmpeg -i $1 -ss $startTime -to $endTime output$index.mp3
+	ffmpeg -i $1 -ss $startTime -to $endTime output$index.mp3 2> /dev/null
 	echo file output$index.mp3 >> edits.txt
 	index=$((index+1))
 	startTime=${ADDR[1]}
 done
 
-ffmpeg -i $1 -ss $startTime output$index.mp3
+ffmpeg -i $1 -ss $startTime output$index.mp3 2> /dev/null
 echo file output$index.mp3 >> edits.txt
 
-ffmpeg -f concat -i edits.txt -c copy final.mp3
+ffmpeg -f concat -i edits.txt -c copy -f final.mp3 2> /dev/null
 
 rm output*mp3
 rm edits.txt
