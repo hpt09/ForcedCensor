ad72f1279004d81d4d596bf071ae4e0e694c7f35
Harpreet <hpt09@live.com>
2019-10-08 13:34:32 +1300

    Added bashcommand that works on trial

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,17 @@
 package sample;
 
+import javafx.event.ActionEvent;
+
 public class Controller {
+
+    BashCommands bash = new BashCommands();
+
+    public void align(ActionEvent event){
+
+        bash.align();
+
+        System.out.println("Hello");
+
+    }
+
 }
