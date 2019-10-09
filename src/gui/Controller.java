de34542fda7a8ed6ed54414aa081e8b05e0aa0e6
Shane <shane.barboza@gmail.com>
2019-10-09 19:44:36 +1300

    Added CensorScreen

--- src/gui/Controller.java
+++ src/gui/Controller.java
@@ -6,6 +6,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
@@ -35,10 +37,24 @@ public class Controller {
     private Button lyricUploadBtn;
 
     @FXML
+    private Button censorBtn;
+
+    @FXML
+    private ToggleGroup censorButtons;
+
+
+    @FXML
     private Label audioFileName;
 
     @FXML
     private Label lyricFileName;
+
+    @FXML
+    private Label warningLabel;
+
+    private String censoringMethod;
+    private File audioFile;
+    private File lyricFile;
 
 
     @FXML
@@ -48,7 +64,6 @@ public class Controller {
 
     @FXML
     public void start() throws IOException {
-
 
         AnchorPane root = FXMLLoader.load(getClass().getResource("AlignScreen.fxml"));
         Main.makeWindowMoveable(root);
@@ -60,34 +75,73 @@ public class Controller {
     @FXML
     public void handleUpload(ActionEvent e) {
         FileChooser fc = new FileChooser();
-        File selectedFile;
+
+        File lyricFile;
 
 
         if (((Button) e.getSource()).getId().equals("audioUploadBtn")) {
             // Audio upload button
             fc.setTitle("Upload your Audio file");
             fc.getExtensionFilters().addAll(new ExtensionFilter("Audio files", "*.wav","*.mp3"));
-            selectedFile = fc.showOpenDialog(null);
+            audioFile = fc.showOpenDialog(null);
 
-            if (selectedFile != null) {
-                audioFileName.setText(selectedFile.getName());
+            if (audioFile != null) {
+                audioFileName.setText(audioFile.getName());
             }
 
         } else {
             // Lyrics upload button
             fc.setTitle("Upload your Lyrics file");
             fc.getExtensionFilters().addAll(new ExtensionFilter("Lyric files", "*.txt"));
-            selectedFile = fc.showOpenDialog(null);
+            lyricFile = fc.showOpenDialog(null);
 
-            if (selectedFile != null) {
-                lyricFileName.setText(selectedFile.getName());
+            if (lyricFile != null) {
+                lyricFileName.setText(lyricFile.getName());
             }
+        }
+    }
+
+    @FXML
+    public void handleAlign() throws IOException {
+
+        AnchorPane root = FXMLLoader.load(getClass().getResource("CensorScreen.fxml"));
+        Main.makeWindowMoveable(root);
+        Main.getPrimaryStage().getScene().setRoot(root);
+
+        // Insert Bash Code here
+
+    }
+
+    @FXML
+    public void handleCensor() {
+        // Get selected toggle
+        if (censorButtons.getSelectedToggle() != null) {
+            censoringMethod = ((ToggleButton)censorButtons.getSelectedToggle()).getText();
+        } else {
+            censoringMethod = null;
+        }
+        // Warn user to select an option
+        if (censoringMethod == null) {
+            warningLabel.setVisible(true);
+            return;
+        } else {
+            warningLabel.setVisible(false);
         }
 
 
+        if (censoringMethod.equals("Filtering")) {
+            // Insert code to do python commands for filtering
+            System.out.println("Insert Filtering commands");
+
+        } else {
+            // Insert code to do python commands for trimming
+            System.out.println("Insert Filtering commands");
+
+        }
+
 
     }
-    
+
 }
 
 
